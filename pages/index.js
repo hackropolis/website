0bfee4b0ac3a3eb9203757344f23ddbcb6873d83
Devnol <pgkoutsoumanis@gmail.com>
2021-01-03 21:02:39 +0200

    revert last commit
    Signed-off-by: Devnol <pgkoutsoumanis@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Heckropolis</title>
+				<title>Hackropolis</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className={styles.main}>
